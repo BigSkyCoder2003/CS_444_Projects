7ced7dcfdf58955c1790b49f63bd088c9427d81c
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-04-06 12:40:59 -0700

    fix hellothread formatting and remove test code (PROJECT 1 FINAL)

--- Project_1/hellothread.c
+++ Project_1/hellothread.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define THREADCOUNT 2
-#define COUNTERUPPERBOUND 25
+#define COUNTERUPPERBOUND 4
 
 void *counter(void *arg)
 {
@@ -16,12 +16,12 @@ for (int i = 0; i < COUNTERUPPERBOUND+1; i++)
 {
   printf("thread %d : %d\n",*threadnum+1,i); 
 }
- return NULL;
+return NULL;
 }
 
 void init_threads(pthread_t threads[], int thread_args[])
 {
-  puts("Launching threads");
+puts("Launching threads");
 
 for (int i = 0; i < THREADCOUNT; i++)
 {
@@ -34,7 +34,7 @@ void join_threads(pthread_t threads[])
 {
 for (int i = 0; i < THREADCOUNT; i++)
 {
-pthread_join(threads[i], NULL);
+  pthread_join(threads[i], NULL);
 }
 puts("Threads complete!");
 }
@@ -47,52 +47,5 @@ init_threads(threads, thread_args);
 join_threads(threads);
 }
 
-
-
-
-
-
-
-
-
-// // Daniel Lounsbury
-// // CS444
-
-// #include <pthread.h>
-// #include <stdio.h>
-
-// #define THREADCOUNT 2
-
-// void *counter(void *arg)
-// {
-
-// int *threadnum = (int *)arg;
-
-// for (int i = 0; i < 5; i++)
-// {
-//   printf("thread %d : %d\n",*threadnum,i); 
-// }
-//  return NULL;
-// }
-
-// int main()
-// {
-// pthread_t threads[THREADCOUNT];
-// int thread_args[THREADCOUNT];
-
-// puts("Launching threads");
-
-// for (int i = 0; i < THREADCOUNT; i++)
-// {
-//   thread_args[i] = i;
-//   pthread_create(&threads[i], NULL, counter, (void*)&thread_args[i]);
-// }
-
-// for (int i = 0; i < THREADCOUNT; i++)
-// {
-// pthread_join(threads[i], NULL);
-// }
-// puts("Threads complete!");
-// }
 
 
