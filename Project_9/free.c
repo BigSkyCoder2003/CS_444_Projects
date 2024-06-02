319236979e36dfd33a5d543bc4e6f92522f1b3ff
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-06-02 03:49:12 -0700

    fix magic numbers (first pass)

--- Project_9/free.c
+++ Project_9/free.c
@@ -3,8 +3,8 @@
 
 void set_free(unsigned char *block, int num, int set)
 {
-  int byte_num = num / 8;
-  int bit_num = num % 8;
+  int byte_num = num / BYTE_SIZE;
+  int bit_num = num % BYTE_SIZE;
 
   if (set)
     block[byte_num] |= (1 << bit_num);
@@ -14,7 +14,7 @@ void set_free(unsigned char *block, int num, int set)
 
 int find_low_clear_bit(unsigned char x)
 {
-  for (int i = 0; i < 8; i++)
+  for (int i = 0; i < BYTE_SIZE; i++)
     if (!(x & (1 << i)))
       return i;
 
@@ -28,7 +28,7 @@ int find_free(unsigned char *block)
   {
     zero_bit = find_low_clear_bit(block[i]);
     if (zero_bit != -1)
-      return i * 8 + zero_bit;
+      return i * BYTE_SIZE + zero_bit;
   }
   return -1;
 }
--- Project_9/free.h
+++ Project_9/free.h
@@ -3,6 +3,7 @@
 
 #define INODE_MAP_BLOCK 1
 #define BLOCK_MAP_BLOCK 2
+#define BYTE_SIZE 8
 
 void set_free(unsigned char *block, int num, int set);
 
